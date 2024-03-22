9d864e547d841b4adac104366f0022a7723eae54
tarasfl <tfliorko@gmail.com>
2024-03-22 19:35:25 +0000

    diable bug fixed

--- src/routes/all-campaigns/[slug]/+page.svelte
+++ src/routes/all-campaigns/[slug]/+page.svelte
@@ -116,28 +116,28 @@
         action="first-page"
         title="First page"
         on:click={() => (currentPage = 0)}
-        disabled={currentPage === 0}>first_page</IconButton
+        >first_page</IconButton
       >
       <IconButton
         class="material-icons"
         action="prev-page"
         title="Prev page"
         on:click={() => currentPage--}
-        disabled={currentPage === 0}>chevron_left</IconButton
+        >chevron_left</IconButton
       >
       <IconButton
         class="material-icons"
         action="next-page"
         title="Next page"
         on:click={() => currentPage++}
-        disabled={currentPage === lastPage}>chevron_right</IconButton
+        >chevron_right</IconButton
       >
       <IconButton
         class="material-icons"
         action="last-page"
         title="Last page"
         on:click={() => (currentPage = lastPage)}
-        disabled={currentPage === lastPage}>last_page</IconButton
+        >last_page</IconButton
       >
     </Pagination>
   </DataTable>
