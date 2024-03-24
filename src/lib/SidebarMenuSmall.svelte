bed4e609d7542cfe3fc34394ac8a49ddd69f0933
tarasfl <tfliorko@gmail.com>
2024-03-24 17:13:58 -0500

    menu icon bug fixed

--- src/lib/SidebarMenuSmall.svelte
+++ src/lib/SidebarMenuSmall.svelte
@@ -8,6 +8,7 @@
   } from '@smui/drawer';
   import List, { Item, PrimaryText, SecondaryText, Graphic} from '@smui/list';
   import Accordion, { Panel, Header, Content as AccordionContent } from '@smui-extra/accordion';
+  import {Icon} from "@smui/common"
 
 
 
@@ -88,8 +89,10 @@
         <Accordion>
           <Panel  variant="unelevated">
             <Header>
-              <Graphic class="material-icons" >{option.icon}</Graphic>
+              <div style = 'display:flex;'>
+              <Icon class="material-icons" >{option.icon}</Icon>
               <SecondaryText><big>{option.title}</big></SecondaryText>
+            </div>
             </Header>
             <AccordionContent>
               <List>
