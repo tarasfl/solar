2b9eef3619f3a9f8227ead4acabb5440161d35bb
tarasfl <tfliorko@gmail.com>
2024-04-01 20:19:31 -0500

    added leads filtering for writing to db

--- src/routes/campaign-result/+page.server.ts
+++ src/routes/campaign-result/+page.server.ts
@@ -1,17 +1,20 @@
-import {getCampaign} from '../../lib/server/db'
+import {getCampaign, getLeadCampaign} from '../../lib/server/db'
 import { error } from '@sveltejs/kit';
 
 export function load() {
     let campaign:any;
+    let leads: any;
     try{
         campaign = getCampaign()
+        leads = getLeadCampaign()
     }
     catch(e){
         throw error(500, 'Failed to load data from database');       
     }
         
     return{
-        campaign: campaign
+        campaign: campaign,
+        leads: leads
     }
 }
 
